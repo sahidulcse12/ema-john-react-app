d5ab78d04d8413596863b5764bbef9466abc5e78
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-02-26 11:15:43 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+    <div>
+      <Header>
+
+      </Header>
     </div>
   );
 }
